1b6b6b1aaccca18fca86cc53adc830a2c4f449fe
levonaramyan <levonaramyan@outlook.com>
2018-10-04 02:49:09 +0400

    New Project: Election Winners

---
+++ electionsWinners/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Elections are in progress!
+// Given an array of the numbers of votes given to each of the candidates so far, and an integer k
+// equal to the number of voters who haven't cast their vote yet, find the number of candidates
+// who still have a chance to win the election.
+
+// The winner of the election must secure strictly more votes than any other candidate.
+// If two or more candidates receive the same (maximum) number of votes, assume there is no winner at all.
+
+// Example:
+// For votes = [2, 3, 5, 2] and k = 3, the output should be: electionsWinners(votes, k) = 2.
+// The first candidate got 2 votes.Even if all of the remaining 3 candidates vote for him,
+//     he will still have only 5 votes, i.e.the same number as the third candidate, so there will be no winner.
+// The second candidate can win if all the remaining candidates vote for him (3 + 3 = 6 > 5).
+// The third candidate can win even if none of the remaining candidates vote for him.
+//     For example, if each of the remaining voters cast their votes for each of his opponents,
+//     he will still be the winner(the votes array will thus be [3, 4, 5, 3]).
+// The last candidate can't win no matter what (for the same reason as the first candidate).
+// RESULT: Thus, only 2 candidates can win(the second and the third), which is the answer.
+
+namespace electionsWinners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializing test values and print the test result
+            int[] test = new int[] { 2, 3, 5, 2 , 4};
+            int k = 3;
+            Console.WriteLine(electionsWinners(test,k));
+            Console.ReadKey();
+
+        }
+
+        // The method returns the number of possible winners in elections
+        static int electionsWinners(int[] votes, int k)
+        {
+            int count = 0; // will be the number of possible winners
+            int maxCounts = 0; // will be the number of current leaders, who have equally maximum votes
+            int maxVotes = votes.Max(); // current maximum votes of a candidate
+
+            // calculates who can beat the maxVotes
+            foreach (int i in votes)
+            {
+                if (i + k > maxVotes) count++;
+                if (i == maxVotes) maxCounts++;
+            }
+
+            if (k == 0 && maxCounts == 1) count = 1; // if the elections were ended, and there was only one with maxVotes
+            return count;
+        }
+    }
+}
