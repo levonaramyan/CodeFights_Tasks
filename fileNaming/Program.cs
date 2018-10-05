87daee15e360f7e6a3f881e642e5a5721caae73c
levonaramyan <levonaramyan@outlook.com>
2018-10-06 03:51:46 +0400

    New Project: File Naming

---
+++ fileNaming/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// You are given an array of desired filenames in the order of their creation.
+// Since two files cannot have equal names, the one which comes later will have
+// an addition to its name in a form of (k), where k is the smallest positive integer
+// such that the obtained name is not used yet.
+
+// PROBLEM: Return an array of names that will be given to the files.
+// Example
+// For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be
+// fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
+
+namespace fileNaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializing a test string array, and printing the result of the test
+            string[] test = new string[] { "doc", "doc", "image", "doc(1)", "doc" };
+            for (int i = 0; i < test.Length; i++) Console.Write($"{test[i]}\t");
+            Console.WriteLine();
+            string[] result = fileNaming(test);
+            for (int i = 0; i < test.Length; i++) Console.Write($"{result[i]}\t");
+            Console.ReadKey();
+
+        }
+
+        // The method returns an array of string, where the names of docs are updated
+        static string[] fileNaming(string[] names)
+        {
+            int length = names.Length; // array length
+            string tempName = ""; // will be a temprorary name of a doc, which name was found as a repeated
+
+            // looking through an array of strings and for each item looking back
+            // and when finding a repeating name, iteratively rename it, until no repeating names found
+            for (int i = 1; i < length; i++)
+            {
+                bool lookBack = true;
+                int k = 0;
+                while (lookBack == true)
+                {
+                    lookBack = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (k == 0) tempName = names[i]; // for ex. doc
+                        if (names[i] == names[j])
+                        {
+                            k++;
+                            names[i] = $"{tempName}({k})"; // will become doc(k)
+                            lookBack = true;
+                        }
+                    }
+                }
+            }
+
+            return names;
+        }
+    }
+}
---
+++ fileNaming/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("fileNaming")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("fileNaming")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea262b2d-76e1-4739-823f-678ba90af318")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
