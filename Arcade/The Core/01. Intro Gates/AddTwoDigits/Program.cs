b4cb23353a0546222e924e418c2dbf99c6a6ecc6
levonaramyan <levonaramyan@gmail.com>
2018-10-15 02:03:00 +0400

    New Project: AddTwoDigits

--- Arcade/The Core/01. Intro Gates/AddTwoDigits/Program.cs
+++ Arcade/The Core/01. Intro Gates/AddTwoDigits/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// You are given a two-digit integer n. Return the sum of its digits.
+
+// Example: For n = 29, the output should be: addTwoDigits(n) = 11.
+
 namespace AddTwoDigits
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Testing and printing the result
+            Console.WriteLine(addTwoDigits(27));
+            Console.ReadKey();
+        }
+        
+        // The method returns a sum of digits in two digit number.
+        static int addTwoDigits(int n)
+        {
+            return n % 10 + n / 10;
         }
     }
 }
