03743499a76280381c9ab623019709d3f55f1e54
levonaramyan <levonaramyan@gmail.com>
2018-10-16 02:17:18 +0400

    New Project: Circle of Numbers

--- Arcade/The Core/01. Intro Gates/CircleOfNumbers/Program.cs
+++ Arcade/The Core/01. Intro Gates/CircleOfNumbers/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance
+// between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+
+// PROBLEM: Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+// Example:
+//         For n = 10 and firstNumber = 2, the output should be
+//         circleOfNumbers(n, firstNumber) = 7.
+
 namespace CircleOfNumbers
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Testing and printing the result
+            Console.WriteLine(circleOfNumbers(10, 2));
+            Console.ReadKey();
+        }
+
+        // Returns the radially opposite number
+        static int circleOfNumbers(int n, int firstNumber)
+        {
+            return (firstNumber + n / 2) % n;
         }
     }
 }
