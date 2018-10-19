db81174fa78278cdc4a0987d85455e0c5e8ee098
levonaramyan <levonaramyan@gmail.com>
2018-10-19 23:39:22 +0400

    New Project: Arithmetic Expression

--- Arcade/The Core/02. At the Crossroads/ArithmeticExpression/Program.cs
+++ Arcade/The Core/02. At the Crossroads/ArithmeticExpression/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Consider an arithmetic expression of the form a#b=c. Check whether it is possible to replace
+// # with one of the four signs: +, -, * or / to obtain a correct expression.
+
 namespace ArithmeticExpression
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(arithmeticExpression(4,5,9));
+            Console.ReadKey();
+        }
+
+        // Returns true if it could be any of the mentioned arithmetic expressions
+        static bool arithmeticExpression(int a, int b, int c)
+        {
+            return (a + b == c || a - b == c || a * b == c || (a / b == c && a % b == 0));
         }
     }
 }
