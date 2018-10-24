b9c6fea32e813c7df8d27bdbea196b34e7cf9f33
levonaramyan <levonaramyan@gmail.com>
2018-10-25 03:13:06 +0400

    New Challenge: Jumping Jimmy

---
+++ Challenges/JumpingJimmy/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Jumping Jimmy is trying to reach the top of a tall tower!
+
+// Given an integer jumpHeight representing the maximum vertical distance he can ascend in one jump,
+// and an array tower representing the vertical distances between each floor of the tower,
+// your task is to find the total vertical distance Jimmy is able to travel
+// (until he reaches the top of the tower or isn't able to complete the next jump).
+
+// Example:
+//          For tower = [3, 1, 2] and jumpHeight = 3, the output should be jumpingJimmy(tower, jumpHeight) = 6.
+//          After reaching the top of the tower, Jimmy has ascended a total distance of 3 + 1 + 2 = 6.
+//
+//          For tower = [1, 2, 3, 4] and jumpHeight = 2, the output should be jumpingJimmy(tower, jumpHeight) = 3.
+//          Jimmy is able to complete the first two jumps, but he can't clear a gap of size 3,
+//          so the total vertical distance is 1 + 2 = 3.
+//
+//          For tower = [5, 1, 8, 2, 4, 3, 1, 9, 8, 5, 1] and jumpHeight = 1,
+//          the output should be jumpingJimmy(tower, jumpHeight) = 0.
+//          Jimmy can't even clear the first gap, so he can't ascend the tower at all
+
+namespace JumpingJimmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializing test tower[] and the height of jump
+            int[] tower = new int[] { 3, 1, 2 };
+            int jumpHeight = 3;
+
+            Console.WriteLine(jumpingJimmy(tower,jumpHeight));
+            Console.ReadKey();
+        }
+
+        static int jumpingJimmy(int[] tower, int jumpHeight)
+        {
+            int dist = 0; // will be the total distance that Jimmy Jumped
+            int floor = 0; // will be the floor that Jimmy reached
+
+            // Jumpitng, until the Jump height is less than the floor height
+            while (floor < tower.Length && tower[floor] <= jumpHeight)
+            {
+                int currentJump = jumpHeight - tower[floor];
+                floor++;
+
+                // if with one jump can pass more than one floor
+                while (floor < tower.Length && tower[floor] <= currentJump)
+                {
+                    currentJump = jumpHeight - tower[floor];
+                    floor++;
+                }
+            }
+
+            // Calculating the total distance that Jimmy was able to jump
+            for (int i = 0; i < floor; i++)
+            {
+                dist += tower[i];
+            }
+
+            return dist;
+
+        }
+    }
+}
---
+++ Challenges/JumpingJimmy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JumpingJimmy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("JumpingJimmy")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("43103eb5-1a44-49fc-8164-01865dd38e47")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
