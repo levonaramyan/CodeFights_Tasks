bf34c2214e83e105ed16f5e587abbb7455c7bdc4
levonaramyan <levonaramyan@gmail.com>
2018-11-02 23:31:42 +0400

    New Project: Create Array

--- Arcade/The Core/05. List Forest Edge/CreateArray/Program.cs
+++ Arcade/The Core/05. List Forest Edge/CreateArray/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given an integer size, return array of length size filled with 1s.
+// Example:
+//          For size = 4, the output should be
+//          createArray(size) = [1, 1, 1, 1].
+
 namespace CreateArray
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Testing and printing the result
+            int[] testRes = createArray(15);
+            foreach (int i in testRes) Console.Write(i + " ");
+            Console.ReadKey();
+        }
+
+        // Returns an array with given number elements, all of which are 1-s
+        static int[] createArray(int size)
+        {
+            int[] res = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                res[i] = 1;
+            }
+
+            return res;
         }
     }
 }
