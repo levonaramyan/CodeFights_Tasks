bda23e3c0ffd704345ba0641d6edcedd936b1f26
levonaramyan <levonaramyan@gmail.com>
2018-11-05 23:48:48 +0400

    New Project: Square Digits Sequence

--- Arcade/The Core/06. Labyrinth of Nested Loops/SquareDigitsSequence/Program.cs
+++ Arcade/The Core/06. Labyrinth of Nested Loops/SquareDigitsSequence/Program.cs
@@ -11,5 +11,39 @@ namespace SquareDigitsSequence
         static void Main(string[] args)
         {
         }
+
+        static int squareDigitsSequence(int a0)
+        {
+            int[] a = new int[30];
+            a[0] = a0;
+            int i = 0;
+            bool isRepeated = false;
+            while (!isRepeated)
+            {
+                i++;
+                a[i] = sumOfDigits(a[i - 1]);
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    if (a[i] == a[j]) isRepeated = true;
+                }
+            }
+
+            return i + 1;
+
+        }
+
+        static int sumOfDigits(int n)
+        {
+            int sum = 0;
+
+            while (n > 0)
+            {
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+
+            return sum;
+        }
+
     }
 }
