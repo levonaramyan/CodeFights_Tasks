f1920613cf2f36bf9d53db6359f4c6b96814cd4d
levonaramyan <levonaramyan@gmail.com>
2018-11-05 23:51:16 +0400

    New Project: Comfortable Numbers

--- Arcade/The Core/06. Labyrinth of Nested Loops/ComfortableNumbers/Program.cs
+++ Arcade/The Core/06. Labyrinth of Nested Loops/ComfortableNumbers/Program.cs
@@ -11,5 +11,40 @@ namespace ComfortableNumbers
         static void Main(string[] args)
         {
         }
+
+        static int comfortableNumbers(int l, int r)
+        {
+            int count = 0;
+            for (int a = l; a < r; a++)
+            {
+                for (int b = a + 1; b <= r; b++)
+                {
+                    if (AreComfortable(a, b)) count++;
+                }
+            }
+
+            return count;
+
+
+        }
+
+        static int DigSum(int a)
+        {
+            int sum = 0;
+            while (a != 0)
+            {
+                sum += a % 10;
+                a /= 10;
+            }
+
+            return sum;
+        }
+
+        static bool AreComfortable(int a, int b)
+        {
+            int aSum = DigSum(a);
+            int bSum = DigSum(b);
+            return (a != b) && (a >= b - bSum) && (a <= b + bSum) && (b >= a - aSum) && (b <= a + aSum);
+        }
     }
 }
