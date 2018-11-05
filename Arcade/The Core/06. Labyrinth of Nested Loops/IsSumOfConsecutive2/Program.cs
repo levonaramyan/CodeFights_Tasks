ca4033fb29d2ed9409140aaed389ac518fcdfb9f
levonaramyan <levonaramyan@gmail.com>
2018-11-05 23:47:15 +0400

    New Project: Is Sum Of Consecutive2

--- Arcade/The Core/06. Labyrinth of Nested Loops/IsSumOfConsecutive2/Program.cs
+++ Arcade/The Core/06. Labyrinth of Nested Loops/IsSumOfConsecutive2/Program.cs
@@ -11,5 +11,23 @@ namespace IsSumOfConsecutive2
         static void Main(string[] args)
         {
         }
+
+        static int isSumOfConsecutive2(int n)
+        {
+            int numOfWays = 0;
+            for (int i = 1; i <= n / 2; i++)
+            {
+                int tempSum = i;
+                int j = i;
+                while (tempSum < n)
+                {
+                    j++;
+                    tempSum += j;
+                    if (tempSum == n) numOfWays++;
+                }
+            }
+
+            return numOfWays;
+        }
     }
 }
