6de16910def95102a083860d7c64f04bf24b4877
levonaramyan <levonaramyan@gmail.com>
2018-11-05 23:57:36 +0400

    New Project: Is Power?

--- Arcade/The Core/06. Labyrinth of Nested Loops/IsPower/Program.cs
+++ Arcade/The Core/06. Labyrinth of Nested Loops/IsPower/Program.cs
@@ -11,5 +11,31 @@ namespace IsPower
         static void Main(string[] args)
         {
         }
+
+        static bool isPower(int n)
+        {
+            bool res = false;
+            for (int i = 2; i <= (int)Math.Sqrt(n); i++)
+            {
+                res = IsPowerOfm(n, i);
+                if (res) break;
+            }
+
+            return n == 1 ? true : res;
+        }
+
+        static bool IsPowerOfm(int n, int m)
+        {
+            bool isPowerOfm = true;
+
+            while (isPowerOfm && n > 1)
+            {
+                if (n % m > 0) isPowerOfm = false;
+                n /= m;
+            }
+
+            return isPowerOfm;
+        }
+
     }
 }
