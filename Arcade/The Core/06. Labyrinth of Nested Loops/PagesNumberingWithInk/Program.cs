af1d899bf6bbb16f21cb819f78236dc10dd1e867
levonaramyan <levonaramyan@gmail.com>
2018-11-05 23:49:56 +0400

    New Project: Pages Numbering With Ink

--- Arcade/The Core/06. Labyrinth of Nested Loops/PagesNumberingWithInk/Program.cs
+++ Arcade/The Core/06. Labyrinth of Nested Loops/PagesNumberingWithInk/Program.cs
@@ -11,5 +11,18 @@ namespace PagesNumberingWithInk
         static void Main(string[] args)
         {
         }
+
+        static int pagesNumberingWithInk(int current, int numberOfDigits)
+        {
+            numberOfDigits -= $"{current}".Length;
+            while (numberOfDigits >= $"{current + 1}".Length)
+            {
+                current++;
+                numberOfDigits -= $"{current}".Length;
+            }
+
+            return current;
+        }
+
     }
 }
