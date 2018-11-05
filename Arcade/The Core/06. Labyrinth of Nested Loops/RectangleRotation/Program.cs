7f8c3fdbdb52e6cb636e29cf28f829632e66f2a3
levonaramyan <levonaramyan@gmail.com>
2018-11-05 23:54:40 +0400

    New Project: Rectangle Rotation

--- Arcade/The Core/06. Labyrinth of Nested Loops/RectangleRotation/Program.cs
+++ Arcade/The Core/06. Labyrinth of Nested Loops/RectangleRotation/Program.cs
@@ -11,5 +11,22 @@ namespace RectangleRotation
         static void Main(string[] args)
         {
         }
+
+        static int rectangleRotation(int a, int b)
+        {
+            double dist = Math.Sqrt(2) / 2;
+            int count = 0;
+            int aNew = (int)(a / (2 * dist));
+            int bNew = (int)(b / (2 * dist));
+            for (int i = -aNew; i <= aNew; i++)
+            {
+                for (int j = -bNew; j <= bNew; j++)
+                {
+                    if (Math.Abs(i) % 2 == Math.Abs(j) % 2) count++;
+                }
+            }
+
+            return count;
+        }
     }
 }
