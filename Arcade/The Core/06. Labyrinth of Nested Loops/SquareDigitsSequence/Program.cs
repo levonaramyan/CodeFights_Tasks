f78875b434b661c46b34dcbac65872c7ae0e8e3e
levonaramyan <levonaramyan@gmail.com>
2018-11-06 00:15:30 +0400

    squareDigitsSequence: Updated comments

--- Arcade/The Core/06. Labyrinth of Nested Loops/SquareDigitsSequence/Program.cs
+++ Arcade/The Core/06. Labyrinth of Nested Loops/SquareDigitsSequence/Program.cs
@@ -4,14 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Consider a sequence of numbers a0, a1, ..., an, in which an element is equal to the sum of
+// squared digits of the previous element. The sequence ends once an element that has already been
+// in the sequence appears again.
+
+// PROBLEM: Given the first element a0, find the length of the sequence.
+// Example:
+//          For a0 = 16, the output should be
+//          squareDigitsSequence(a0) = 9.
+//          Here's how elements of the sequence are constructed:
+
+//          a0 = 16
+//          a1 = 12 + 62 = 37
+//          a2 = 32 + 72 = 58
+//          a3 = 52 + 82 = 89
+//          a4 = 82 + 92 = 145
+//          a5 = 12 + 42 + 52 = 42
+//          a6 = 42 + 22 = 20
+//          a7 = 22 + 02 = 4
+//          a8 = 42 = 16, which has already occurred before (a0)
+//          Thus, there are 9 elements in the sequence.
+
+//          For a0 = 103, the output should be
+//          squareDigitsSequence(a0) = 4.
+//          The sequence goes as follows: 103 -> 10 -> 1 -> 1, 4 elements altogether.
+
 namespace SquareDigitsSequence
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(squareDigitsSequence(16));
+            Console.ReadKey();
         }
 
+        // Returns the number of elements of square digits sequence, starting from a0
         static int squareDigitsSequence(int a0)
         {
             int[] a = new int[30];
@@ -32,6 +60,7 @@ namespace SquareDigitsSequence
 
         }
 
+        // Returns the sum of digits squares
         static int sumOfDigits(int n)
         {
             int sum = 0;
