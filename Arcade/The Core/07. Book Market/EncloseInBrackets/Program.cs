f8c21c6bec61488d9e0fee2c97ea597883a56e9b
levonaramyan <levonaramyan@gmail.com>
2019-01-10 14:32:01 +0400

    EncloseInBrackets: Added the solution

--- Arcade/The Core/07. Book Market/EncloseInBrackets/Program.cs
+++ Arcade/The Core/07. Book Market/EncloseInBrackets/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given a string, enclose it in round brackets.
+// Example:
+//          For inputString = "abacaba", the output should be
+//          encloseInBrackets(inputString) = "(abacaba)".
+
 namespace EncloseInBrackets
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Testing and printing the result
+            Console.WriteLine(encloseInBrackets("abds"));
+            Console.ReadKey();
+        }
+
+        // Returns a new string, which contains an inputString in brackets
+        static string encloseInBrackets(string inputString)
+        {
+            return $"({inputString})";
         }
     }
 }
