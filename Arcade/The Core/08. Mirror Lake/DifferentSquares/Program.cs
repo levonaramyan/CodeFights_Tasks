04a46f47f497a856843995fa8c9bf7dee09a59f2
levonaramyan <levonaramyan@gmail.com>
2019-01-12 14:43:03 +0400

    Different Squares: Added test and comments

--- Arcade/The Core/08. Mirror Lake/DifferentSquares/Program.cs
+++ Arcade/The Core/08. Mirror Lake/DifferentSquares/Program.cs
@@ -4,19 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
+// Example
+//          For
+//          matrix = [[1, 2, 1],
+//                    [2, 2, 2],
+//                    [2, 2, 2],
+//                    [1, 2, 3],
+//                    [2, 2, 1]]
+//          the output should be
+//          differentSquares(matrix) = 6.
+//          Here are all 6 different 2 × 2 squares:
+//          1 2
+//          2 2
+//
+//          2 1
+//          2 2
+//
+//          2 2
+//          2 2
+//
+//          2 2
+//          1 2
+//
+//          2 2
+//          2 3
+//
+//          2 3
+//          2 1
+
 namespace DifferentSquares
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            // Testing and printing the result
+            int[][] test = new int[5][];
+            test[0] = new int[] { 1, 2, 1 };
+            test[1] = new int[] { 2, 2, 2 };
+            test[2] = new int[] { 2, 2, 2 };
+            test[3] = new int[] { 1, 2, 3 };
+            test[4] = new int[] { 2, 2, 1 };
+            Console.WriteLine(differentSquares(test));
+            Console.ReadKey();
         }
 
         static int differentSquares(int[][] matrix)
         {
-            int difMatNum = 0;
+            int difMatNum = 0; // The number of duplicating submatrixes
 
+            // Mtrix, which indicates whether the element is new (0), or has a duplicate before it (1) 
             int y = matrix[0].Length;
             int x = matrix.Length;
             int[][] eqMatrix = new int[x - 1][];
@@ -25,6 +63,9 @@ namespace DifferentSquares
                 eqMatrix[i] = new int[y - 1];
             }
 
+            // If any two 2x2 elements in matrix are equal, then mark with 1 the position of the repeating element
+            // For each [i,j] element, look only the elements which comes after it, i.e.
+            // [i,j+1],[i,j+2],....,[i+1][0],....,[x-1,y-1]
             for (int i = 0; i < x - 1; i++)
             {
                 for (int j = 0; j < y - 1; j++)
@@ -45,6 +86,7 @@ namespace DifferentSquares
                 }
             }
 
+            // Calculating the number of duplicates
             for (int i = 0; i < x - 1; i++)
             {
                 for (int j = 0; j < y - 1; j++)
@@ -56,6 +98,7 @@ namespace DifferentSquares
             return ((x - 1) * (y - 1) - difMatNum);
         }
 
+        // Returns 1, if 2x2 submatrixes with [i,j] and [k,h] indexes are equal, else returns 0.
         static int twoDimmEquality(int[][] matrix, int i, int j, int k, int h)
         {
             int areEqual = 0;
