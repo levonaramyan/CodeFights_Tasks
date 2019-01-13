f7f0b663e605ed2f79eb9d44acf4a99d2dd989ac
levonaramyan <levonaramyan@gmail.com>
2019-01-13 12:54:12 +0400

    New Project: House of Cats (only code)

---
+++ Arcade/The Core/09. Well of Integration/HouseOfCats/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseOfCats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int[] houseOfCats(int legs)
+        {
+            int variants = legs / 4 + 1;
+            int[] numOfPeople = new int[variants];
+            for (int i = 0; i <= legs / 4; i++)
+            {
+                numOfPeople[variants - 1 - i] = (legs - i * 4) / 2;
+            }
+
+            return numOfPeople;
+        }
+
+    }
+}
