ab65bcb09dcc181d0ecd5bd85bdda33c95c5081c
levonaramyan <levonaramyan@gmail.com>
2019-01-13 12:46:19 +0400

    New Project: House Numbers Sum (only code)

---
+++ Arcade/The Core/09. Well of Integration/HouseNumbersSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseNumbersSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int houseNumbersSum(int[] inputArray)
+        {
+            int i = 0;
+            int sum = 0;
+            while (inputArray[i] > 0)
+            {
+                sum += inputArray[i];
+                i++;
+            }
+
+            return sum;
+
+        }
+    }
+}
