cf6ccdb08d1e397291d4750686915e717468e7cd
levonaramyan <levonaramyan@gmail.com>
2019-01-13 12:59:09 +0400

    Alphabet subsequence: Added the description of the task

--- Arcade/The Core/09. Well of Integration/AlphabetSubsequence/Program.cs
+++ Arcade/The Core/09. Well of Integration/AlphabetSubsequence/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Check whether the given string is a subsequence of the plaintext alphabet.
+// Example:
+//          For s = "effg" or s = "cdce", the output should be
+//          alphabetSubsequence(s) = false;
+//
+//          For s = "ace" or s = "bxz", the output should be
+//          alphabetSubsequence(s) = true.
+
 namespace AlphabetSubsequence
 {
     class Program
