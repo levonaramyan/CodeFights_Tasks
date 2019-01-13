75f4d2d98ea497f6e48cd2f0e058085a989bd89b
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:12:08 +0400

    Switch lights: Added the description of the task

--- Arcade/The Core/09. Well of Integration/SwitchLights/Program.cs
+++ Arcade/The Core/09. Well of Integration/SwitchLights/Program.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// N candles are placed in a row, some of them are initially lit. For each candle
+// from the 1st to the Nth the following algorithm is applied: if the observed candle
+// is lit then states of this candle and all candles before it are changed to the opposite.
+// Which candles will remain lit after applying the algorithm to all candles
+// in the order they are placed in the line?
+// Example:
+//          For a = [1, 1, 1, 1, 1], the output should be
+//          switchLights(a) = [0, 1, 0, 1, 0].
+//
+//          For a = [0, 0], the output should be
+//          switchLights(a) = [0, 0].
+//          The candles are not initially lit, so their states are not altered by the algorithm.
+
 namespace SwitchLights
 {
     class Program
