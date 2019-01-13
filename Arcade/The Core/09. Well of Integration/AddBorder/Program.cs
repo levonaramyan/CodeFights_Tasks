c0e11456a11be9f356243879ff875a4f18991420
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:05:50 +0400

    New Project: Add border (only code)

---
+++ Arcade/The Core/09. Well of Integration/AddBorder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddBorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string[] addBorder(string[] picture)
+        {
+            string[] newStr = new String[picture.Length + 2];
+            for (int i = 0; i < picture.Length; i++)
+            {
+                picture[i] = $"*{picture[i]}*";
+                newStr[i + 1] = picture[i];
+            }
+
+            int strLen = picture[0].Length;
+            string line = new String('*', strLen);
+            newStr[0] = line;
+            newStr[newStr.Length - 1] = line;
+
+            return newStr;
+
+        }
+    }
+}
