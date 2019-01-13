b5f9d9355475dc1ee4b31749be2e95044e259948
levonaramyan <levonaramyan@gmail.com>
2019-01-13 12:57:37 +0400

    New Project: Alphabet subsequence (only code)

---
+++ Arcade/The Core/09. Well of Integration/AlphabetSubsequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool alphabetSubsequence(string s)
+        {
+            int len = s.Length;
+            bool isAlph = true;
+            for (int i = 1; isAlph && i < len; i++)
+            {
+                if (s[i] <= s[i - 1]) isAlph = false;
+            }
+
+            return isAlph;
+
+        }
+    }
+}
