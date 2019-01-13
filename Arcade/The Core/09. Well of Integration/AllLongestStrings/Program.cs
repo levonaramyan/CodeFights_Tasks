ebf27f6494e46c430aa12095f16b126351336122
levonaramyan <levonaramyan@gmail.com>
2019-01-13 12:52:45 +0400

    All longest strings: Added the desciption of the task

--- Arcade/The Core/09. Well of Integration/AllLongestStrings/Program.cs
+++ Arcade/The Core/09. Well of Integration/AllLongestStrings/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given an array of strings, return another array containing all of its longest strings.
+// Example:
+//          For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+//          allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
 namespace AllLongestStrings
 {
     class Program
