6cfb335798980fefaa9844e025497335925aae75
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:29:17 +0400

    Are Similar?: added the description of the task

--- Arcade/The Core/09. Well of Integration/AreSimilar/Program.cs
+++ Arcade/The Core/09. Well of Integration/AreSimilar/Program.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Two arrays are called similar if one can be obtained from another by swapping at most one pair
+// of elements in one of the arrays.
+// PROBLEM: Given two arrays a and b, check whether they are similar.
+// Example:
+//          For a = [1, 2, 3] and b = [1, 2, 3], the output should be
+//          areSimilar(a, b) = true.
+//          The arrays are equal, no need to swap any elements.
+//
+//          For a = [1, 2, 3] and b = [2, 1, 3], the output should be
+//          areSimilar(a, b) = true.
+//          We can obtain b from a by swapping 2 and 1 in b.
+//
+//          For a = [1, 2, 2] and b = [2, 1, 1], the output should be
+//          areSimilar(a, b) = false.
+//          Any swap of any two elements either in a or in b won't make a and b equal.
+
 namespace AreSimilar
 {
     class Program
