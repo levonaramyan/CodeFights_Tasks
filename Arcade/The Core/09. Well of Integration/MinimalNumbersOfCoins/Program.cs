e72e2a04e51cc29b38a6e338592af79de53b762c
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:01:03 +0400

    New Project: Minimal Number of Coins (only code)

---
+++ Arcade/The Core/09. Well of Integration/MinimalNumbersOfCoins/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalNumbersOfCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int minimalNumberOfCoins(int[] coins, int price)
+        {
+            int coinNum = 0;
+            int index = 0;
+            while (index < coins.Length && price > coins[index])
+            {
+                index++;
+            }
+
+            index--;
+
+            while (price > 0)
+            {
+                coinNum += price / coins[index];
+                price = price % coins[index];
+                index--;
+            }
+
+            return coinNum;
+
+        }
+
+    }
+}
