8547097b8e799a22df43ad7908457b8c142d181d
levonaramyan <levonaramyan@gmail.com>
2019-01-13 12:55:48 +0400

    House of cats: Added the description of the task

--- Arcade/The Core/09. Well of Integration/HouseOfCats/Program.cs
+++ Arcade/The Core/09. Well of Integration/HouseOfCats/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// There are some people and cats in a house. You are given the number of legs they have all together.
+// Your task is to return an array containing every possible number of people that could be
+// in the house sorted in ascending order. It's guaranteed that each person has 2 legs and each cat has 4 legs.
+// Example:
+//          For legs = 6, the output should be
+//          houseOfCats(legs) = [1, 3].
+//          There could be either 1 cat and 1 person(4 + 2 = 6) or 3 people(2 * 3 = 6).
+//
+//          For legs = 2, the output should be
+//          houseOfCats(legs) = [1].
+//          There can be only 1 person.
+
 namespace HouseOfCats
 {
     class Program
