c380392b49a7d8e637ab2a3d92d030ce9b253239
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:25:48 +0400

    IntegerToStringOfFixedWidth: Added the description of the task

--- Arcade/The Core/09. Well of Integration/IntegerToStringOfFixedWidth/Program.cs
+++ Arcade/The Core/09. Well of Integration/IntegerToStringOfFixedWidth/Program.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given a positive integer number and a certain length, we need to modify the given number
+// to have a specified length. We are allowed to do that either by cutting out leading digits
+// (if the number needs to be shortened) or by adding 0s in front of the original number.
+// Example:
+//          For number = 1234 and width = 2, the output should be
+//          integerToStringOfFixedWidth(number, width) = "34";
+//
+//          For number = 1234 and width = 4, the output should be
+//          integerToStringOfFixedWidth(number, width) = "1234";
+//
+//          For number = 1234 and width = 5, the output should be
+//          integerToStringOfFixedWidth(number, width) = "01234".
+
 namespace IntegerToStringOfFixedWidth
 {
     class Program
