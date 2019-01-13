4d82d422065697d633f4f49b7493e3019375f507
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:19:34 +0400

    New Project: Elections Winner (only code)

---
+++ Arcade/The Core/09. Well of Integration/ElectionsWinner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionsWinner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int electionsWinners(int[] votes, int k)
+        {
+            int count = 0;
+            int maxCounts = 0;
+            int maxVotes = votes.Max();
+
+            foreach (int i in votes)
+            {
+                if (i + k > maxVotes) count++;
+                if (i == maxVotes) maxCounts++;
+            }
+
+            if (k == 0 && maxCounts == 1) count = 1;
+            return count;
+
+        }
+    }
+}
