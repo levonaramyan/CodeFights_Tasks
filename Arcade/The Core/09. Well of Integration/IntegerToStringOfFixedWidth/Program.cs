b2a0b4a7520019a58ea68b1bf2274c9d935b9636
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:23:21 +0400

    New Project: integer to string of fixed width (only code)

---
+++ Arcade/The Core/09. Well of Integration/IntegerToStringOfFixedWidth/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerToStringOfFixedWidth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string integerToStringOfFixedWidth(int number, int width)
+        {
+            return ($"{number}".Length >= width) ? $"{number}".Substring($"{number}".Length - width)
+                : (new String('0', (width - $"{number}".Length)) + $"{number}");
+
+        }
+    }
+}
