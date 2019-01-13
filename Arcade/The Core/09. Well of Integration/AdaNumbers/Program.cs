838d1b2d8df37696b8897c5cf2b85983fe00137b
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:31:02 +0400

    New Project: Ada Numbers (only code)

---
+++ Arcade/The Core/09. Well of Integration/AdaNumbers/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool adaNumber(string line)
+        {
+            bool atLeastOneDigit = false;
+            int len = line.Length;
+            if (line[len - 1] == '#')
+            {
+                int i = 0;
+                int theBase = 0;
+
+                while (line[i] != '#' && theBase <= 16)
+                {
+                    if (line[i] != '_')
+                    {
+                        if ('0' <= line[i] && line[i] <= '9')
+                        {
+                            theBase = theBase * 10 + (line[i] - '0');
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    i++;
+                }
+                if (theBase < 2 || theBase > 16)
+                {
+                    return false;
+                }
+                i++;
+
+                while (i < len - 1)
+                {
+                    if (line[i] != '_')
+                    {
+                        var digit = -1;
+                        if ('a' <= line[i] && line[i] <= 'f')
+                        {
+                            digit = line[i] - 'a' + 10;
+                        }
+                        if ('A' <= line[i] && line[i] <= 'F')
+                        {
+                            digit = line[i] - 'A' + 10;
+                        }
+                        if ('0' <= line[i] && line[i] <= '9')
+                        {
+                            digit = line[i] - '0';
+                        }
+                        if (0 <= digit && digit < theBase)
+                        {
+                            atLeastOneDigit = true;
+                        }
+                        else return false;
+                    }
+                    i++;
+                }
+            }
+            else
+            {
+                foreach (char i in line)
+                {
+                    if (i != '_')
+                    {
+                        if ('0' <= i && i <= '9')
+                        {
+                            atLeastOneDigit = true;
+                        }
+                        else return false;
+                    }
+                }
+            }
+            return atLeastOneDigit;
+        }
+    }
+}
