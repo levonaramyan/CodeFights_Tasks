0570941c55dd170fe331ee01a6c744c813acc0ae
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:33:47 +0400

    Ada Number: added the description of the task

--- Arcade/The Core/09. Well of Integration/AdaNumbers/Program.cs
+++ Arcade/The Core/09. Well of Integration/AdaNumbers/Program.cs
@@ -4,6 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Consider two following representations of a non-negative integer:
+// A simple decimal integer, constructed of a non-empty sequence of digits from 0 to 9;
+// An integer with at least one digit in a base from 2 to 16 (inclusive),
+// enclosed between # characters, and preceded by the base, which can only be a number
+// between 2 and 16 in the first representation. For digits from 10 to 15
+// characters a, b, ..., f and A, B, ..., F are used.
+// Additionally, both representations may contain underscore(_) characters;
+// they are used only as separators for improving legibility of numbers and can be ignored while processing a number.
+// PROBLEM: Your task is to determine whether the given string is a valid integer representation.
+// Note: this is how integer numbers are represented in the programming language Ada.
+// Example:
+//          For line = "123_456_789", the output should be
+//          adaNumber(line) = true;
+//
+//          For line = "16#123abc#", the output should be
+//          adaNumber(line) = true;
+//
+//          For line = "10#123abc#", the output should be
+//          adaNumber(line) = false;
+//
+//          For line = "10#10#123ABC#", the output should be
+//          adaNumber(line) = false;
+//
+//          For line = "10#0#", the output should be
+//          adaNumber(line) = true;
+//
+//          For line = "10##", the output should be
+//          adaNumber(line) = false.
+
 namespace AdaNumbers
 {
     class Program
