ace6f318d650f1c23a6b52236f6792fe60245705
levonaramyan <levonaramyan@gmail.com>
2019-01-13 13:10:03 +0400

    New Project: Switch lights (only code)

---
+++ Arcade/The Core/09. Well of Integration/SwitchLights/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchLights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int[] switchLights(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == 1)
+                {
+                    for (int j = i; j >= 0; j--)
+                    {
+                        a[j] = 1 - a[j];
+                    }
+                }
+            }
+
+            return a;
+
+        }
+    }
+}
