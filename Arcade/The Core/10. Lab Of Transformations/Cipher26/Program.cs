69ec780997e92fbb0590c0b7c23317fcfb67349c
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:15:41 +0400

    Cipher26: Added the description of the task

--- Arcade/The Core/10. Lab Of Transformations/Cipher26/Program.cs
+++ Arcade/The Core/10. Lab Of Transformations/Cipher26/Program.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// You've intercepted an encrypted message, and you are really curious about its contents.
+// You were able to find out that the message initially contained only lowercase English letters,
+// and was encrypted with the following cipher:
+// Let all letters from 'a' to 'z' correspond to the numbers from 0 to 25, respectively.
+// The number corresponding to the ith letter of the encrypted message is then equal to the sum
+// of numbers corresponding to the first i letters of the initial unencrypted message modulo 26.
+// Now that you know how the message was encrypted, implement the algorithm to decipher it.
+// Example:
+//          For message = "taiaiaertkixquxjnfxxdh", the output should be
+//          cipher26(message) = "thisisencryptedmessage".
+//          The initial message "thisisencryptedmessage" was encrypted as follows:
+//          letter 0: t -> 19 -> t;
+//          letter 1: th -> (19 + 7) % 26 -> 0 -> a;
+//          letter 2: thi -> (19 + 7 + 8) % 26 -> 8 -> i;
+//          etc.
+
 namespace Cipher26
 {
     class Program
