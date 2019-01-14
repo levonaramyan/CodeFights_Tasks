0fdcbeb11c43cef985460f1ccfcf65726b7d5ab5
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:19:02 +0400

    Stolen Lunch: added the description of the task

--- Arcade/The Core/10. Lab Of Transformations/StolenLunch/Program.cs
+++ Arcade/The Core/10. Lab Of Transformations/StolenLunch/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// When you recently visited your little nephew, he told you a sad story: there's a bully
+// at school who steals his lunch every day, and locks it away in his locker.
+// He also leaves a note with a strange, coded message. Your nephew gave you one of the notes
+// in hope that you can decipher it for him. And you did: it looks like all the digits in it
+// are replaced with letters and vice versa. Digit 0 is replaced with 'a', 1 is replaced with 'b'
+// and so on, with digit 9 replaced by 'j'.
+// The note is different every day, so you decide to write a function that will decipher it
+// for your nephew on an ongoing basis.
+// Example:
+//          For note = "you'll n4v4r 6u4ss 8t: cdja", the output should be
+//          stolenLunch(note) = "you'll never guess it: 2390".
+
 namespace StolenLunch
 {
     class Program
