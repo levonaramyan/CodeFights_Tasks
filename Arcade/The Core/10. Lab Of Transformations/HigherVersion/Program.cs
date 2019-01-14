cf9a772132569bcd990a451db18cf4cdabe77e5b
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:20:30 +0400

    New Project: Higher Version (only code)

---
+++ Arcade/The Core/10. Lab Of Transformations/HigherVersion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigherVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool higherVersion(string ver1, string ver2)
+        {
+            bool isHigher = true;
+            int num1 = 0;
+            int num2 = 0;
+            while (isHigher && ver1.Length > 0)
+            {
+                num1 = (ver1.Contains('.')) ? int.Parse(ver1.Remove(ver1.IndexOf('.'))) : int.Parse(ver1);
+                num2 = (ver2.Contains('.')) ? int.Parse(ver2.Remove(ver2.IndexOf('.'))) : int.Parse(ver2);
+                if (num1 > num2) break;
+                if (num2 > num1) isHigher = false;
+                ver1 = (ver1.Contains('.')) ? ver1.Substring(ver1.IndexOf('.') + 1) : "";
+                ver2 = (ver2.Contains('.')) ? ver2.Substring(ver2.IndexOf('.') + 1) : "";
+            }
+            if (num1 == num2 && isHigher) isHigher = false;
+            return isHigher;
+        }
+    }
+}
