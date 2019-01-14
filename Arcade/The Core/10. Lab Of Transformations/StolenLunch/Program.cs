ef2d78ee778c1ae2e05290e33d1c304286040b75
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:17:06 +0400

    New project: Stolen Lunch (only code)

---
+++ Arcade/The Core/10. Lab Of Transformations/StolenLunch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StolenLunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string stolenLunch(string note)
+        {
+            string res = "";
+            char newChar = ' ';
+            foreach (char i in note)
+            {
+                if (char.IsDigit(i)) newChar = (char)('a' + int.Parse($"{i}"));
+                else if (char.IsLetter(i) && i <= 'j' && i >= 'a') newChar = (char)('0' + i - 'a');
+                else newChar = i;
+                res += newChar;
+            }
+
+            return res;
+
+        }
+    }
+}
