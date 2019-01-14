10b526a552c3da6dc0af53ec9bb3acdc2ef54043
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:06:12 +0400

    New Project: Reflect String (only code)

---
+++ Arcade/The Core/10. Lab Of Transformations/ReflectString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string reflectString(string inputString)
+        {
+            string reflected = "";
+            foreach (char i in inputString)
+            {
+                reflected += (char)('z' + 'a' - i);
+            }
+            return reflected;
+
+        }
+    }
+}
