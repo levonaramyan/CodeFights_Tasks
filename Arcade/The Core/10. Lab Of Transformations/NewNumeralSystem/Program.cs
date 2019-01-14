d6fb03341ece7f8bc1f5a8fa7d987cbf133f75da
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:09:29 +0400

    New Project: New Numeral System (only code)

---
+++ Arcade/The Core/10. Lab Of Transformations/NewNumeralSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewNumeralSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string[] newNumeralSystem(char number)
+        {
+            int numDec = number - 'A';
+            string[] res = new string[numDec / 2 + 1];
+            for (int i = 0; i <= numDec / 2; i++)
+            {
+                res[i] = $"{(char)('A' + i)} + {(char)('A' + numDec - i)}";
+            }
+
+            return res;
+        }
+    }
+}
