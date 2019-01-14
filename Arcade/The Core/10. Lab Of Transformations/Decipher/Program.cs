a1547afbadcc958e44dba87ff5e40b746da1afc0
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:26:04 +0400

    Decipher: added the description of the task

--- Arcade/The Core/10. Lab Of Transformations/Decipher/Program.cs
+++ Arcade/The Core/10. Lab Of Transformations/Decipher/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Consider the following ciphering algorithm:
+//      For each character replace it with its code.
+//      Concatenate all of the obtained numbers.
+// Given a ciphered string, return the initial one if it is known that it consists only of lowercase letters.
+// Note: here the character's code means its decimal ASCII code, the numerical representation
+// of a character used by most modern programming languages.
+// Example:
+//          For cipher = "10197115121", the output should be
+//          decipher(cipher) = "easy".
+//          Explanation: charCode('e') = 101, charCode('a') = 97, charCode('s') = 115 and charCode('y') = 121.
+
 namespace Decipher
 {
     class Program
