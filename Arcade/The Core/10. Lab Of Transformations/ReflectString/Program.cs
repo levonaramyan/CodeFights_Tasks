346d80f30d9fd077bda0d703470866a01736cda7
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:07:43 +0400

    Reflect String: Added the description of the task

--- Arcade/The Core/10. Lab Of Transformations/ReflectString/Program.cs
+++ Arcade/The Core/10. Lab Of Transformations/ReflectString/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Define an alphabet reflection as follows: a turns into z, b turns into y, c turns 
+// into x, ..., n turns into m, m turns into n, ..., z turns into a.
+// Define a string reflection as the result of applying the alphabet reflection to each of its characters.
+// Reflect the given string.
+// Example:
+//          For inputString = "name", the output should be
+//          reflectString(inputString) = "mznv".
+
 namespace ReflectString
 {
     class Program
