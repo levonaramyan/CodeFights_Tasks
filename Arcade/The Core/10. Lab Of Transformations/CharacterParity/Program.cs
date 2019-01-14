53186552eea2fd59614bdddd6956acb1d0d86a17
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:04:11 +0400

    Character Parity: added the description of the task

--- Arcade/The Core/10. Lab Of Transformations/CharacterParity/Program.cs
+++ Arcade/The Core/10. Lab Of Transformations/CharacterParity/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given a character, check if it represents an odd digit, an even digit or not a digit at all.
+// Example:
+//          For symbol = '5', the output should be
+//          characterParity(symbol) = "odd";
+//
+//          For symbol = '8', the output should be
+//          characterParity(symbol) = "even";
+//
+//          For symbol = 'q', the output should be
+//          characterParity(symbol) = "not a digit".
+
 namespace CharacterParity
 {
     class Program
