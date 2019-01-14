9214aafcb16945f148cec999e7f1d4211e7fd5d5
levonaramyan <levonaramyan@gmail.com>
2019-01-14 23:02:44 +0400

    New Project: Character Parity (only code)

---
+++ Arcade/The Core/10. Lab Of Transformations/CharacterParity/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterParity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string characterParity(char symbol)
+        {
+            return (!char.IsDigit(symbol)) ? "not a digit" : ((symbol % 2 == 0) ? "even" : "odd");
+
+        }
+    }
+}
