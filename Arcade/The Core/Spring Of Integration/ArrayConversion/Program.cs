06ae9d190fb8d9692375358e20a153395edbdd7c
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:46:40 +0400

    New Project: Array Conversion (only code)

---
+++ Arcade/The Core/Spring Of Integration/ArrayConversion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int arrayConversion(int[] inputArray)
+        {
+            bool iter = false;
+            while (inputArray.Length > 1)
+            {
+                inputArray = RecConv(inputArray, iter);
+                iter = !iter;
+            }
+
+            return inputArray[0];
+
+        }
+
+        static int[] RecConv(int[] arr, bool even)
+        {
+            int[] conv = new int[arr.Length / 2];
+            for (int i = 0; i < conv.Length; i++) conv[i] = even ? arr[2 * i] * arr[2 * i + 1] : arr[2 * i] + arr[2 * i + 1];
+            return conv;
+        }
+    }
+}
---
+++ Arcade/The Core/Spring Of Integration/ArrayConversion/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArrayConversion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ArrayConversion")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("330bc20b-f702-4e36-9d0c-19b1470a11a9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
