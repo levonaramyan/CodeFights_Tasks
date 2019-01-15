6a0a400a452518de156480bca3a75d3499418515
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:59:08 +0400

    New project: strings crossover (only code)

---
+++ Arcade/The Core/Spring Of Integration/StringsCrossover/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsCrossover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int stringsCrossover(string[] inputArray, string result)
+        {
+            int c = 0;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+                for (int j = i + 1; j < inputArray.Length; j++)
+                    if (IsGoodPair(inputArray[i], inputArray[j], result)) c++;
+            return c;
+        }
+
+        static bool IsGoodPair(string a, string b, string s)
+        {
+            bool isOk = true;
+            for (int i = 0; isOk && i < a.Length; i++) if (s[i] != a[i] && s[i] != b[i]) isOk = false;
+            return isOk;
+        }
+    }
+}
