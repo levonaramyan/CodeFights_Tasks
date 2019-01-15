826a4f550496285e5d3df55c2256f524542c2415
levonaramyan <levonaramyan@gmail.com>
2019-01-16 00:11:20 +0400

    RunnersMeetings: added the description of the task

--- Arcade/The Core/Spring Of Integration/RunnersMeetings/Program.cs
+++ Arcade/The Core/Spring Of Integration/RunnersMeetings/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+// Some people run along a straight line in the same direction. They start simultaneously at
+// pairwise distinct positions and run with constant speed (which may differ from person to person).
+// If two or more people are at the same point at some moment we call that a meeting.
+// The number of people gathered at the same point is called meeting cardinality.
+// For the given starting positions and speeds of runners find the maximum meeting
+// cardinality assuming that people run infinitely long. If there will be no meetings, return -1 instead.
+// Example:
+//          For startPosition = [1, 4, 2] and speed = [27, 18, 24], the output should be
+//          runnersMeetings(startPosition, speed) = 3.
+//          In 20 seconds after the runners start running, they end up at the same point.
 
 namespace RunnersMeetings
 {
