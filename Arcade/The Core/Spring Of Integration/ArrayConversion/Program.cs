168b0825ed6998d91a4ec1e447a5a0c178aff8cc
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:48:19 +0400

    ArrayConversion: added the description of the task

--- Arcade/The Core/Spring Of Integration/ArrayConversion/Program.cs
+++ Arcade/The Core/Spring Of Integration/ArrayConversion/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given an array of 2k integers (for some integer k), perform the following operations until
+// the array contains only one element:
+// On the 1st, 3rd, 5th, etc.iterations(1-based) replace each pair of consecutive elements with their sum;
+// On the 2nd, 4th, 6th, etc.iterations replace each pair of consecutive elements with their product.
+// After the algorithm has finished, there will be a single element left in the array. Return that element.
+// Example:
+//          For inputArray = [1, 2, 3, 4, 5, 6, 7, 8], the output should be
+//          arrayConversion(inputArray) = 186.
+//          We have [1, 2, 3, 4, 5, 6, 7, 8] -> [3, 7, 11, 15] -> [21, 165] -> [186], so the answer is 186.
+
 namespace ArrayConversion
 {
     class Program
