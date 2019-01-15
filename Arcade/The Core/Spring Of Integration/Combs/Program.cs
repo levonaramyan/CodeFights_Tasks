0bd7d125a493c82d9c4747f087ef80f0817bcd94
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:57:26 +0400

    Combs: added the description of the task

--- Arcade/The Core/Spring Of Integration/Combs/Program.cs
+++ Arcade/The Core/Spring Of Integration/Combs/Program.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Miss X has only two combs in her possession, both of which are old and miss a tooth or two.
+// She also has many purses of different length, in which she carries the combs.
+// The only way they fit is horizontally and without overlapping. Given teeth' positions on both combs,
+// find the minimum length of the purse she needs to take them with her.
+// It is guaranteed that there is at least one tooth at each end of the comb.
+// It is also guaranteed that the total length of two strings is smaller than 32.
+// Note, that the combs can not be rotated/reversed.
+// Example:
+//          For comb1 = "*..*" and comb2 = "*.*", the output should be
+//          combs(comb1, comb2) = 5.
+
+
+
 namespace Combs
 {
     class Program
