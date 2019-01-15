300d247dfa7a8ac07e2a7450e8269a2f11f1ce21
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:54:15 +0400

    New Project: Added the description of the task

--- Arcade/The Core/Spring Of Integration/PairOfShoes/Program.cs
+++ Arcade/The Core/Spring Of Integration/PairOfShoes/Program.cs
@@ -4,6 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Yesterday you found some shoes in the back of your closet. Each shoe is described by two values:
+// type indicates if it's a left or a right shoe;
+// size is the size of the shoe.
+// Your task is to check whether it is possible to pair the shoes you found in such a way
+// that each pair consists of a right and a left shoe of an equal size.
+// Example:
+//          For
+//          shoes = [[0, 21],
+//                   [1, 23],
+//                   [1, 21],
+//                   [0, 23]]
+//          the output should be
+//          pairOfShoes(shoes) = true;
+//
+//          For
+//          shoes = [[0, 21],
+//                   [1, 23],
+//                   [1, 21],
+//                   [1, 23]]
+//          the output should be
+//          pairOfShoes(shoes) = false.
+
 namespace PairOfShoes
 {
     class Program
