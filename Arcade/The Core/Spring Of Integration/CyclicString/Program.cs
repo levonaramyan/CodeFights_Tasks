d302db7c412022e6e3ca51e4f174c50bfad3b334
levonaramyan <levonaramyan@gmail.com>
2019-01-16 00:04:18 +0400

    Cyclic String: added the description of the task

--- Arcade/The Core/Spring Of Integration/CyclicString/Program.cs
+++ Arcade/The Core/Spring Of Integration/CyclicString/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// You're given a substring s of some cyclic string. What's the length of the smallest possible
+// string that can be concatenated to itself many times to obtain this cyclic string?
+// Example:
+//          For s = "cabca", the output should be
+//          cyclicString(s) = 3.
+//          "cabca" is a substring of a cycle string "abcabcabcabc..." that can be obtained
+//          by concatenating "abc" to itself.Thus, the answer is 3.
+
 namespace CyclicString
 {
     class Program
