63435b8766f5b123e107a0f4f04aaa134ebb7b78
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:52:11 +0400

    New Project: Pair of Shoes (only code)

---
+++ Arcade/The Core/Spring Of Integration/PairOfShoes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairOfShoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool pairOfShoes(int[][] shoes)
+        {
+            bool isPossible = false;
+            for (int i = 0; i < shoes.Length; i++)
+            {
+                isPossible = false;
+                for (int j = 0; j < shoes.Length; j++)
+                {
+                    if (shoes[i][0] == 1 - shoes[j][0] && shoes[i][1] == shoes[j][1])
+                    {
+                        isPossible = true;
+                        shoes[i][1] = 0;
+                        shoes[j][1] = 0;
+                        break;
+                    }
+                }
+                if (!isPossible) break;
+            }
+
+            return shoes.Length % 2 != 0 ? false : isPossible;
+
+        }
+
+    }
+}
