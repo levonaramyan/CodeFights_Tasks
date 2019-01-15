a701c45591b9772c2d3f1e1a36387bbfde5677a2
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:50:44 +0400

    Array Previous Less: added the description of the task

--- Arcade/The Core/Spring Of Integration/ArrayPreviousLess/Program.cs
+++ Arcade/The Core/Spring Of Integration/ArrayPreviousLess/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given array of integers, for each position i, search among the previous positions for the last
+// (from the left) position that contains a smaller value. Store this value at position i in the answer.
+// If no such value can be found, store -1 instead.
+// Example:
+//          For items = [3, 5, 2, 4, 5], the output should be
+//          arrayPreviousLess(items) = [-1, 3, -1, 2, 4].
+
 namespace ArrayPreviousLess
 {
     class Program
