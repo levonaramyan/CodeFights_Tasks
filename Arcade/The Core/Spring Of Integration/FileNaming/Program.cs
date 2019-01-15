df986d95c27b2c369e66dc359c31de1d57809b2d
levonaramyan <levonaramyan@gmail.com>
2019-01-16 00:21:51 +0400

    FileNaming: added the description of the task

--- Arcade/The Core/Spring Of Integration/FileNaming/Program.cs
+++ Arcade/The Core/Spring Of Integration/FileNaming/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// You are given an array of desired filenames in the order of their creation.
+// Since two files cannot have equal names, the one which comes later will have an addition
+// to its name in a form of (k), where k is the smallest positive integer
+// such that the obtained name is not used yet.
+// PROBLEM: Return an array of names that will be given to the files.
+// Example:
+//          For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be
+//          fileNaming(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
+
 namespace FileNaming
 {
     class Program
