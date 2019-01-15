d1aea96b6d07576121c2c5da38eb93adde411b39
levonaramyan <levonaramyan@gmail.com>
2019-01-16 00:01:14 +0400

    Strings Crossover: added the description of the task

--- Arcade/The Core/Spring Of Integration/StringsCrossover/Program.cs
+++ Arcade/The Core/Spring Of Integration/StringsCrossover/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Define crossover operation over two equal-length strings A and B as follows:
+//      the result of that operation is a string of the same length as the input strings
+//      result[i] is either A[i] or B[i], chosen at random
+// Given array of strings inputArray and a string result, find for how many pairs of strings
+// from inputArray the result of the crossover operation over them may be equal to result.
+// Note that (A, B) and (B, A) are the same pair.Also note that the pair cannot include
+// the same element of the array twice (however, if there are two equal elements
+// in the array, they can form a pair).
+// Example:
+//          For inputArray = ["abc", "aaa", "aba", "bab"] and result = "bbb", the output should be
+//          stringsCrossover(inputArray, result) = 2.
+
 namespace StringsCrossover
 {
     class Program
