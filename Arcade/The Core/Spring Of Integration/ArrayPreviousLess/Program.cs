b3c0c76bfc279dc7ac4be7d5c1beac53bd84507c
levonaramyan <levonaramyan@gmail.com>
2019-01-15 23:49:41 +0400

    New Project: Array Previous Less (only code)

---
+++ Arcade/The Core/Spring Of Integration/ArrayPreviousLess/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPreviousLess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int[] arrayPreviousLess(int[] items)
+        {
+            int[] res = new int[items.Length];
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = -1;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (items[j] < items[i])
+                    {
+                        res[i] = items[j];
+                        break;
+                    }
+                }
+            }
+
+            return res;
+
+        }
+
+    }
+}
