8a647304db16dd03466c18e0342e3ea4753de887
levonaramyan <levonaramyan@gmail.com>
2019-01-20 23:39:14 +0400

    ExtractMatrixColumn: added the description of the task

--- Arcade/The Core/12. List Backwoods/ExractMatrixColumn/Program.cs
+++ Arcade/The Core/12. List Backwoods/ExractMatrixColumn/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given a rectangular matrix and an integer column, return an array containing the elements
+// of the columnth column of the given matrix (the leftmost column is the 0th one).
+// Example:
+//          For
+//          matrix = [[1, 1, 1, 2], 
+//                    [0, 5, 0, 4], 
+//                    [2, 1, 3, 6]]
+//          and column = 2, the output should be
+//          extractMatrixColumn(matrix, column) = [1, 0, 3].
+
 namespace ExractMatrixColumn
 {
     class Program
