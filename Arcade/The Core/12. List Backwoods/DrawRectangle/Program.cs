7171014ee541a02b37312af12bcf7dec392f9aee
levonaramyan <levonaramyan@gmail.com>
2019-01-21 00:04:31 +0400

    DrawRectangle: added the description of the task

--- Arcade/The Core/12. List Backwoods/DrawRectangle/Program.cs
+++ Arcade/The Core/12. List Backwoods/DrawRectangle/Program.cs
@@ -4,6 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// You are implementing a command-line version of the Paint app.Since the command line doesn't
+// support colors, you are using different characters to represent pixels. Your current goal
+// is to support rectangle x1 y1 x2 y2 operation, which draws a rectangle that has an
+// upper left corner at (x1, y1) and a lower right corner at (x2, y2).
+// Here the x-axis points from left to right, and the y-axis points from top to bottom.
+// PROBLEM: Given the initial canvas state and the array that represents the coordinates
+//          of the two corners, return the canvas state after the operation is applied.
+//          For the details about how rectangles are painted, see the example.
+// Example:
+//          For
+//          canvas = [['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+//                    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+//                    ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+//                    ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
+//                    ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b']]
+//          and rectangle = [1, 1, 4, 3], the output should be
+//          drawRectangle(canvas, rectangle) = [['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+//                                              ['a', '*', '-', '-', '*', 'a', 'a', 'a'],
+//                                              ['a', '|', 'a', 'a', '|', 'a', 'a', 'a'],
+//                                              ['b', '*', '-', '-', '*', 'b', 'b', 'b'],
+//                                              ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b']]
+//          Note that rectangle sides are depicted as -s and |s, asterisks(*) stand for
+//          its corners and all of the other "pixels" remain the same.Color in the example
+//          is used only for illustration.
+
 namespace DrawRectangle
 {
     class Program
