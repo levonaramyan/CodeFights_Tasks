7c0ce7f84ea990b2aac76af44e3eb9ae5ea26d19
levonaramyan <levonaramyan@gmail.com>
2019-01-20 23:40:38 +0400

    New Project: Are Isomorphic? (only code)

---
+++ Arcade/The Core/12. List Backwoods/AreIsomorphic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreIsomorphic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool areIsomorphic(int[][] array1, int[][] array2)
+        {
+            int x1 = array1.Length;
+            int x2 = array2.Length;
+            bool areIsom = true;
+            if (x1 != x2) return false;
+            else
+                for (int i = 0; areIsom && i < x1; i++)
+                    if (array1[i].Length != array2[i].Length) areIsom = false;
+            return areIsom;
+        }
+    }
+}
