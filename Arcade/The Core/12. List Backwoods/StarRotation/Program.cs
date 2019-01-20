97312d29642cc761a54b5ad79bbee1adc8861ada
levonaramyan <levonaramyan@gmail.com>
2019-01-21 00:15:31 +0400

    StarRotation: Added the description of the task

--- Arcade/The Core/12. List Backwoods/StarRotation/Program.cs
+++ Arcade/The Core/12. List Backwoods/StarRotation/Program.cs
@@ -4,6 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Consider a (2k+1) × (2k+1) square subarray of an integer integers matrix.
+// Let's call the union of the square's two longest diagonals, middle column and middle row a star.
+// Given the coordinates of the star's center in the matrix and its width, rotate it 45 · t
+// degrees clockwise preserving position of all matrix elements that do not belong to the star.
+// Example:
+//          For
+//          matrix = [[1, 0, 0, 2, 0, 0, 3],
+//                    [0, 1, 0, 2, 0, 3, 0],
+//                    [0, 0, 1, 2, 3, 0, 0],
+//                    [8, 8, 8, 9, 4, 4, 4],
+//                    [0, 0, 7, 6, 5, 0, 0],
+//                    [0, 7, 0, 6, 0, 5, 0],
+//                    [7, 0, 0, 6, 0, 0, 5]]
+//          width = 7, center = [3, 3] and t = 1, the output should be
+//          starRotation(matrix, width, center, t) = [[8, 0, 0, 1, 0, 0, 2],
+//                                                    [0, 8, 0, 1, 0, 2, 0],
+//                                                    [0, 0, 8, 1, 2, 0, 0],
+//                                                    [7, 7, 7, 9, 3, 3, 3],
+//                                                    [0, 0, 6, 5, 4, 0, 0],
+//                                                    [0, 6, 0, 5, 0, 4, 0],
+//                                                    [6, 0, 0, 5, 0, 0, 4]]
+//
+//          For
+//          matrix = [[1, 0, 0, 2, 0, 0, 3],
+//                    [0, 1, 0, 2, 0, 3, 0],
+//                    [0, 0, 1, 2, 3, 0, 0],
+//                    [8, 8, 8, 9, 4, 4, 4],
+//                    [0, 0, 7, 6, 5, 0, 0]]
+//          width = 3, center = [1, 5] and t = 81, the output should be
+//          starRotation(matrix, width, center, t) = [[1, 0, 0, 2, 0, 0, 0],
+//                                                    [0, 1, 0, 2, 3, 3, 3],
+//                                                    [0, 0, 1, 2, 0, 0, 0],
+//                                                    [8, 8, 8, 9, 4, 4, 4],
+//                                                    [0, 0, 7, 6, 5, 0, 0]]
+
+
 namespace StarRotation
 {
     class Program
