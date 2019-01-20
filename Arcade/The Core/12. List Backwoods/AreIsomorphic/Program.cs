6196d660a3321b53a972a52e20d6caf27f61a4bd
levonaramyan <levonaramyan@gmail.com>
2019-01-20 23:43:01 +0400

    AreIsomorphic: added the description of the task

--- Arcade/The Core/12. List Backwoods/AreIsomorphic/Program.cs
+++ Arcade/The Core/12. List Backwoods/AreIsomorphic/Program.cs
@@ -4,6 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Two two-dimensional arrays are isomorphic if they have the same number of rows
+// and each pair of respective rows contains the same number of elements.
+// PROBLEM: Given two two-dimensional arrays, check if they are isomorphic.
+// Example:
+//          For
+//          array1 = [[1, 1, 1],
+//                    [0, 0]]
+//          and
+//          array2 = [[2, 1, 1],
+//                    [2, 1]]
+//          the output should be
+//          areIsomorphic(array1, array2) = true;
+//
+//          For
+//          array1 = [[2],
+//                    []]
+//          and
+//          array2 = [[2]]
+//          the output should be
+//          areIsomorphic(array1, array2) = false.
+
 namespace AreIsomorphic
 {
     class Program
