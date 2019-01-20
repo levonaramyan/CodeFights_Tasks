79d260002a188d142672fe05289bb1850ee717f5
levonaramyan <levonaramyan@gmail.com>
2019-01-20 23:55:33 +0400

    New Project: Swap Diagonals (only code)

---
+++ Arcade/The Core/12. List Backwoods/SwapDiagonals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapDiagonals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int[][] swapDiagonals(int[][] matrix)
+        {
+            int len = matrix.Length;
+            int[][] res = new int[len][];
+            for (int i = 0; i < len; i++)
+            {
+                res[i] = new int[len];
+                for (int j = 0; j < len; j++)
+                    res[i][j] = matrix[i][j];
+                res[i][i] = matrix[i][len - 1 - i];
+                res[i][len - 1 - i] = matrix[i][i];
+            }
+            return res;
+
+        }
+
+    }
+}
