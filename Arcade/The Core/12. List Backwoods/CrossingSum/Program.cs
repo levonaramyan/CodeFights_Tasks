3b25923d5ad301d67e3f3cb97ca8e6903f80a965
levonaramyan <levonaramyan@gmail.com>
2019-01-20 23:58:27 +0400

    New Project: Crossing Sum (only code)

---
+++ Arcade/The Core/12. List Backwoods/CrossingSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossingSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int crossingSum(int[][] matrix, int a, int b)
+        {
+            int x = matrix.Length;
+            int y = matrix[0].Length;
+            int sum = -matrix[a][b];
+            for (int i = 0; i < x; i++)
+                sum += matrix[i][b];
+            for (int i = 0; i < y; i++)
+                sum += matrix[a][i];
+            return sum;
+
+        }
+    }
+}
