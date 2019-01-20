3a28c1b57b32212c666cd2578b7879d6122a5ece
levonaramyan <levonaramyan@gmail.com>
2019-01-20 23:57:12 +0400

    SwapDiagonals: added the description of the task

--- Arcade/The Core/12. List Backwoods/SwapDiagonals/Program.cs
+++ Arcade/The Core/12. List Backwoods/SwapDiagonals/Program.cs
@@ -4,6 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// The longest diagonals of a square matrix are defined as follows:
+// the first longest diagonal goes from the top left corner to the bottom right one;
+// the second longest diagonal goes from the top right corner to the bottom left one.
+// Given a square matrix, your task is to swap its longest diagonals by exchanging their elements
+// at the corresponding positions.
+// Example:
+//          For
+//          matrix = [[1, 2, 3],
+//                    [4, 5, 6],
+//                    [7, 8, 9]]
+//          the output should be
+//          swapDiagonals(matrix) = [[3, 2, 1],
+//                                   [4, 5, 6],
+//                                   [9, 8, 7]]
+
 namespace SwapDiagonals
 {
     class Program
