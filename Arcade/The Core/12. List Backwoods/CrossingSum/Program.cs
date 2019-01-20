e5446defe6b861d5d03412f58c89a4d1b4935b7b
levonaramyan <levonaramyan@gmail.com>
2019-01-20 23:59:45 +0400

    Crossing Sum: added the description of the task

--- Arcade/The Core/12. List Backwoods/CrossingSum/Program.cs
+++ Arcade/The Core/12. List Backwoods/CrossingSum/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given a rectangular matrix and integers a and b, consider the union of the ath row
+// and the bth (both 0-based) column of the matrix (i.e. all cells that belong either
+// to the ath row or to the bth column, or to both). Return sum of all elements of that union.
+// Example:
+//          For
+//          matrix = [[1, 1, 1, 1], 
+//                    [2, 2, 2, 2], 
+//                    [3, 3, 3, 3]]
+//          a = 1, and b = 3, the output should be
+//          crossingSum(matrix, a, b) = 12.
+//          Here(2 + 2 + 2 + 2) + (1 + 3) = 12.
+
 namespace CrossingSum
 {
     class Program
