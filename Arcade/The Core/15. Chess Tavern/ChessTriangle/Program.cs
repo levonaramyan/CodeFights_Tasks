4998a4fc2ba1de0470bd7183fc893a1dac4d72c7
levonaramyan <levonaramyan@gmail.com>
2019-01-29 00:34:23 +0400

    ChessTriangle: added the description of the task

--- Arcade/The Core/15. Chess Tavern/ChessTriangle/Program.cs
+++ Arcade/The Core/15. Chess Tavern/ChessTriangle/Program.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Consider a bishop, a knight and a rook on an n × m chessboard. They are said to form a triangle
+// if each piece attacks exactly one other piece and is attacked by exactly one piece.
+// Calculate the number of ways to choose positions of the pieces to form a triangle.
+// Note that the bishop attacks pieces sharing the common diagonal with it; the rook attacks
+// in horizontal and vertical directions; and, finally, the knight attacks squares which are
+// two squares horizontally and one square vertically, or two squares vertically
+// and one square horizontally away from its position.
+// https://codefightsuserpics.s3.amazonaws.com/tasks/chessTriangle/img/moves.png?_tm=1490625690642
+// Example:
+//          For n = 2 and m = 3, the output should be
+//          chessTriangle(n, m) = 8.
+//          https://codefightsuserpics.s3.amazonaws.com/tasks/chessTriangle/img/combinations.png?_tm=1490625690826
+
 namespace ChessTriangle
 {
     class Program
