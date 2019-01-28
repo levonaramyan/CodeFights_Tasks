595fbf14eb8e5258b7683914b3181c390782439c
levonaramyan <levonaramyan@gmail.com>
2019-01-28 23:55:28 +0400

    New Project: Bishop and Pawn

---
+++ Arcade/The Core/15. Chess Tavern/BishopAndPawn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BishopAndPawn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool bishopAndPawn(string bishop, string pawn)
+        {
+            int xBish = bishop[0] - 'a' + 1;
+            int yBish = Convert.ToInt32($"{bishop[1]}");
+            int xPawn = pawn[0] - 'a' + 1;
+            int yPawn = Convert.ToInt32($"{pawn[1]}");
+
+            return (Math.Abs(xBish - xPawn) == Math.Abs(yBish - yPawn));
+        }
+    }
+}
