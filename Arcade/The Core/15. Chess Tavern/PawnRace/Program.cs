df1a164a62f5eec1215de17bd469e25e6b95815d
levonaramyan <levonaramyan@gmail.com>
2019-01-29 00:44:20 +0400

    PawnRace: added the description of the task

--- Arcade/The Core/15. Chess Tavern/PawnRace/Program.cs
+++ Arcade/The Core/15. Chess Tavern/PawnRace/Program.cs
@@ -4,6 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Pawn race is a game for two people, played on an ordinary 8 × 8 chessboard.
+// The first player has a white pawn, the second one - a black pawn. Initially
+// the pawns are placed somewhere on the board so that the 1st and the 8th rows are not occupied.
+// Players take turns to make a move.
+// White pawn moves upwards, black one moves downwards.The following moves are allowed:
+//      one-cell move on the same vertical in the allowed direction;
+//      two-cell move on the same vertical in the allowed direction, if the pawn is standing
+//      on the 2nd(for the white pawn) or the 7th(for the black pawn) row.Note that even with the
+//      two-cell move a pawn can't jump over the opponent's pawn;
+//      capture move one cell forward in the allowed direction and one cell to the left or to the right.
+// https://codefightsuserpics.s3.amazonaws.com/tasks/pawnRace/img/move_types.png?_tm=1530802148779
+// The purpose of the game is to reach the the 1st row(for the black pawn) or the 8th row(for the white one),
+// or to capture the opponent's pawn.
+// Given the initial positions and whose turn it is, determine who will win
+// or declare it a draw(i.e.it is impossible for any player to win). Assume that the players play optimally.
+// Example:
+//          For white = "e2", black = "e7", and toMove = 'w', the output should be
+//          pawnRace(white, black, toMove) = "draw";
+//
+//          For white = "e3", black = "d7", and toMove = 'b', the output should be
+//          pawnRace(white, black, toMove) = "black";
+//          
+//          For white = "a7", black = "h2", and toMove = 'w', the output should be
+//          pawnRace(white, black, toMove) = "white".
+
 namespace PawnRace
 {
     class Program
