f3e71aa635ab0e9d1a647cf57ae166c6df09c66c
levonaramyan <levonaramyan@gmail.com>
2019-01-29 00:39:49 +0400

    AmazonCheckMate: added the description of the task

--- Arcade/The Core/15. Chess Tavern/AmazonCheckmate/Program.cs
+++ Arcade/The Core/15. Chess Tavern/AmazonCheckmate/Program.cs
@@ -4,6 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// An amazon (also known as a queen+knight compound) is an imaginary chess piece that can move
+// like a queen or a knight (or, equivalently, like a rook, bishop, or knight).
+// The diagram below shows all squares which the amazon attacks from e4 
+// circles represent knight-like moves while crosses correspond to queen-like moves).
+// https://codefightsuserpics.s3.amazonaws.com/tasks/amazonCheckmate/img/amazon.png?_tm=1490625459392
+// Recently you've come across a diagram with only three pieces left on the board:
+// a white amazon, white king and black king. It's black's move. You don't have time
+// to determine whether the game is over or not, but you'd like to figure it out in your head.
+// Unfortunately, the diagram is smudged and you can't see the position of the black's king,
+// so it looks like you'll have to check them all.
+// Given the positions of white pieces on a standard chessboard, determine
+// the number of possible black king's positions such that:
+//      it's checkmate (i.e. black's king is under amazon's attack and it cannot make a valid move);
+//      it's check (i.e. black's king is under amazon's attack but it can reach a safe square in one move);
+//      it's stalemate (i.e. black's king is on a safe square but it cannot make a valid move);
+//      black's king is on a safe square and it can make a valid move.
+// Note that two kings cannot be placed on two adjacent squares(including two diagonally adjacent ones).
+// Example:
+//          For king = "d3" and amazon = "e4", the output should be
+//          amazonCheckmate(king, amazon) = [5, 21, 0, 29].
+//          https://codefightsuserpics.s3.amazonaws.com/tasks/amazonCheckmate/img/example1.png?_tm=1490625459585
+//          Red crosses correspond to the checkmate positions, orange pluses refer to checks
+//          and green circles denote safe squares.
+//
+//          For king = "a1" and amazon = "g5", the output should be
+//          amazonCheckmate(king, amazon) = [0, 29, 1, 29].
+//          https://codefightsuserpics.s3.amazonaws.com/tasks/amazonCheckmate/img/example2.png?_tm=1490625459719
+//          Stalemate position is marked by a blue square.
+
 namespace AmazonCheckmate
 {
     class Program
