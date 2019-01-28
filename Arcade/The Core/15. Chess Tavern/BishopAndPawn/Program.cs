0fb82214d536d071a0ff46039d9aefdc47dbd700
levonaramyan <levonaramyan@gmail.com>
2019-01-28 23:57:51 +0400

    BishopAndPawn: added the description of the task

--- Arcade/The Core/15. Chess Tavern/BishopAndPawn/Program.cs
+++ Arcade/The Core/15. Chess Tavern/BishopAndPawn/Program.cs
@@ -4,6 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Given the positions of a white bishop and a black pawn on the standard chess board,
+// determine whether the bishop can capture the pawn in one move.
+// The bishop has no restrictions in distance for each move, but is limited to diagonal movement.
+// Check out the example below to see how it can move:
+// https://codefightsuserpics.s3.amazonaws.com/tasks/bishopAndPawn/img/bishop.jpg?_tm=1530791225817
+// Example:
+//          For bishop = "a1" and pawn = "c3", the output should be
+//          bishopAndPawn(bishop, pawn) = true.
+//          https://codefightsuserpics.s3.amazonaws.com/tasks/bishopAndPawn/img/ex1.jpg?_tm=1530791226122
+//
+//          For bishop = "h1" and pawn = "h3", the output should be
+//          bishopAndPawn(bishop, pawn) = false.
+//          https://codefightsuserpics.s3.amazonaws.com/tasks/bishopAndPawn/img/ex2.jpg?_tm=1530791226426
+
+
+
 namespace BishopAndPawn
 {
     class Program
