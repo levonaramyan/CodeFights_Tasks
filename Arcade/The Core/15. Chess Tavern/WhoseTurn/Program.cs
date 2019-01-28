361f6c64196635af136724d6ecfc76b661712b0a
levonaramyan <levonaramyan@gmail.com>
2019-01-29 00:26:27 +0400

    WhoseTurn: added the description of the task

--- Arcade/The Core/15. Chess Tavern/WhoseTurn/Program.cs
+++ Arcade/The Core/15. Chess Tavern/WhoseTurn/Program.cs
@@ -4,6 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Imagine a standard chess board with only two white and two black knights placed in their
+// standard starting positions: the white knights on b1 and g1; the black knights on b8 and g8.
+// https://codefightsuserpics.s3.amazonaws.com/tasks/whoseTurn/img/initial_pos.png?_tm=1530820966293
+// There are two players: one plays for white, the other for black.During each move,
+// the player picks one of his knights and moves it to an unoccupied square according
+// to standard chess rules.Thus, a knight on d5 can move to any of the following squares:
+// b6, c7, e7, f6, f4, e3, c3, and b4, as long as it is not occupied by either a friendly or an enemy knight.
+// https://codefightsuserpics.s3.amazonaws.com/tasks/whoseTurn/img/knight.jpg?_tm=1530820966615
+// The players take turns in making moves, starting with the white player.
+// Given the configuration p of the knights after an unspecified number of moves, determine whose turn it is.
+// Example:
+//          For p = "b1;g1;b8;g8", the output should be
+//          whoseTurn(p) = true.
+//          The configuration corresponds to the initial state of the game. Thus, it's white's turn.
+
 namespace WhoseTurn
 {
     class Program
