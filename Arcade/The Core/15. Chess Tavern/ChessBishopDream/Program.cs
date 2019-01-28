06a7653fbc64c7a2e0d491dcaf2b94ad77a2a77c
levonaramyan <levonaramyan@gmail.com>
2019-01-29 00:30:27 +0400

    ChessBishopDream: added the description of the task

--- Arcade/The Core/15. Chess Tavern/ChessBishopDream/Program.cs
+++ Arcade/The Core/15. Chess Tavern/ChessBishopDream/Program.cs
@@ -4,6 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// In ChessLand there is a small but proud chess bishop with a recurring dream.
+// In the dream the bishop finds itself on an n × m chessboard with mirrors along each edge,
+// and it is not a bishop but a ray of light. This ray of light moves only along diagonals
+// (the bishop can't imagine any other types of moves even in its dreams), it never stops,
+// and once it reaches an edge or a corner of the chessboard it reflects from it and moves on.
+// Given the initial position and the direction of the ray, find its position
+// after k steps where a step means either moving from one cell to the neighboring one
+// or reflecting from a corner of the board.
+// Example:
+//          For boardSize = [3, 7], initPosition = [1, 2],
+//          initDirection = [-1, 1], and k = 13, the output should be
+//          chessBishopDream(boardSize, initPosition, initDirection, k) = [0, 1].
+//          Here is the bishop's path:
+//          [1, 2] -> [0, 3] -(reflection from the top edge)-> [0, 4] -> 
+//          [1, 5] -> [2, 6] -(reflection from the bottom right corner)-> [2, 6] ->
+//          [1, 5] -> [0, 4] -(reflection from the top edge)-> [0, 3] ->
+//          [1, 2] -> [2, 1] -(reflection from the bottom edge)-> [2, 0] -(reflection from the left edge)->
+//          [1, 0] -> [0, 1]
+//          https://codefightsuserpics.s3.amazonaws.com/tasks/chessBishopDream/img/example.png?_tm=1530791315311
+
 namespace ChessBishopDream
 {
     class Program
