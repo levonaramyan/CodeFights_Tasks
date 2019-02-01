30c9a850bc17b07bea54ba547d09d9ca824281c7
levonaramyan <levonaramyan@gmail.com>
2019-02-01 17:08:57 +0400

    New Project: replace all digits with regular expressions

---
+++ Arcade/The Core/17. Regular Hell/ReplaceAllDigitsRegExp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+// Implement a function that replaces each digit in the given string with a '#' character.
+// Example:
+//          For input = "There are 12 points", the output should be
+//          replaceAllDigitsRegExp(input) = "There are ## points".
+
+namespace ReplaceAllDigitsRegExp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string replaceAllDigitsRegExp(string input)
+        {
+            return Regex.Replace(input, @"\d", "#");
+        }
+    }
+}
